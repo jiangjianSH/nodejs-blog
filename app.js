c28356ed8b9959ead4efbbbdd80f7ed8f82955d6
Jiang Jian <jian.jiang@dodopipe.com>
2016-01-22 15:23:46 +0800

    add monogdb support

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest1');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,8 +25,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('/home/jiangjian/test/js/node/express_example/public'));
+app.use(function(req, res, next) {
+	req.db = db;
+	next();
+});
 app.use('/', routes);
 app.use('/users', users);
 
